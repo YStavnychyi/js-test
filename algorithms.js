c14b83fcb282f73ee0fb5a102c076358f00c96c4
Yevhenii <zhestav20@gmail.com>
2022-01-18 22:16:04 +0100

    Algorithms and tests for them.

--- algorithms.js
+++ algorithms.js
@@ -18,16 +18,88 @@ export const palindrome = (inputString) =>{
 // fn(‘silevis’) = ‘l’;
 // fn(‘aabbbcdde’ ) = ’c’;
 // fn(‘eelloo’ ) = ’_’
-
 export const findUniqueChar = (inputString) =>{
     for (let i = 0; i < inputString.length; i++){
         let letter = inputString.charAt(i)
         if(inputString.indexOf(letter) == i && inputString.indexOf(letter, i+1) == -1){
             return letter
         }
-        // console.log(inputString.indexOf(letter), i, inputString.indexOf(letter,i+1))
+        //console.log(inputString.indexOf(letter), i, inputString.indexOf(letter,i+1))
     }
     return '-'
 }
+// //console.log(findUniqueChar('silevis'))
 
+// 3. Dana jest tablica unikalnych, nieujemnych, całkowitych liczb. Wykonaj funkcję, która zwraca ilość elementów, które po posortowaniu tablicy powinny się dodać, aby nie było przerw między kolejnymi liczbami.
+//     Przykład:
+// fn( [5,2,9,7]) = 4; // [2, 3, 4, 5, 6, 7, 8, 9];
+// fn( [1, 2, 3]) = 0; // [1, 2, 3];
+// fn( [4, 9]) = 4; // [4, 5, 6, 7, 8, 9];
+// fn( [1]) = 0; // [1].
+export const numAddElem = (array) =>{
+    let newArray = []
+    if(!array){
+        return 'Array not created!'
+    }else {
+        array.sort((a,b) =>{
+            return a - b
+        })
+    }
+    //console.log(array[array.length-1])
+    for(let i = array[0]; i <= array[array.length - 1]; i++){
+        newArray.push([i])
+    }
+    // console.log(newArray)
+    return newArray.length - array.length
+}
+//console.log(numAddElem([5,2,9,7]))
 
+// 4. Mapowanie z płaskiej struktury drzewiastej na zagnieżdżoną.
+// const flatTree = [
+//     { id: 1, parentId: null },
+//     { id: 2, parentId: 1 },
+//     { id: 3, parentId: 1 },
+//     { id: 4, parentId: 3 } ];
+export const flatTreeToNestedTree = (items, id=null, link = 'parentId') =>{
+    return items
+        .filter(item => item[link] === id)
+        .map(item => ({ ...item, children: flatTreeToNestedTree(items, item.id) }));
+}
+
+// 5. Mapowanie z zagnieżdżonej struktury drzewiastej na płaską.
+// const expectedData = [
+//     {
+//         "id": 1,
+//         "parentId": null,
+//         "children": [
+//             {
+//                 "children": [],
+//                 "id": 2,
+//                 "parentId": 1
+//             },
+//             {
+//                 "id": 3,
+//                 "parentId": 1,
+//                 "children": [
+//                     {
+//                         "children": [],
+//                         "id": 4,
+//                         "parentId": 3
+//                     }
+//                 ]
+//             }
+//         ]
+//     }
+// ]
+export const nestedTreeToFlatTree = obj => {
+    const array = Array.isArray(obj) ? obj : [obj]
+    return array.reduce((acc,value)=>{
+        acc.push(value)
+        if(value.children){
+            acc = acc.concat(nestedTreeToFlatTree(value.children));
+            delete value.children
+        }
+        return acc
+    },[])
+}
+// //console.log(nestedTreeToFlatTree(expectedData))
--- algorithms.test.js
+++ algorithms.test.js
@@ -1,5 +1,5 @@
 import {
-    findUniqueChar,
+    findUniqueChar, flatTreeToNestedTree, nestedTreeToFlatTree, numAddElem,
     palindrome
 
 
@@ -28,19 +28,111 @@ describe('algorithms', () =>{
 
     it('should return a unique character if one exists', () => {
         const inputString = findUniqueChar('silevis')
-        const expeccted = 'l'
-        expect(inputString).toEqual(expeccted)
+        const expected = 'l'
+        expect(inputString).toEqual(expected)
     });
 
     it('should return a unique character if one exists', () => {
         const inputString = findUniqueChar('aabbbcdde')
-        const expeccted = 'c'
-        expect(inputString).toEqual(expeccted)
+        const expected = 'c'
+        expect(inputString).toEqual(expected)
     });
 
     it('should return a sign if all characters were repeating.', () => {
         const inputString = findUniqueChar('eelloo')
-        const expeccted = '-'
-        expect(inputString).toEqual(expeccted)
+        const expected = '-'
+        expect(inputString).toEqual(expected)
+    });
+
+    it('should return the number of elements was added to the sorted array so that there are no gaps between the numbers', () => {
+        const inputArray = numAddElem([5,2,9,7])
+        const expected = 4
+        expect(inputArray).toEqual(expected)
+    });
+
+    it('should return an error message if the function arguments are not specified', () => {
+        const inputArray = numAddElem()
+        expect(inputArray).toBe('Array not created!')
+    });
+
+    it('should return the nested tree structure', () => {
+        const data = [
+            { id: 1, parentId: null },
+            { id: 2, parentId: 1 },
+            { id: 3, parentId: 1 },
+            { id: 4, parentId: 3 } ];
+
+        const expectedData = [
+            {
+                "id": 1,
+                "parentId": null,
+                "children": [
+                    {
+                        "children": [],
+                        "id": 2,
+                        "parentId": 1
+                    },
+                    {
+                        "id": 3,
+                        "parentId": 1,
+                        "children": [
+                            {
+                                "children": [],
+                                "id": 4,
+                                "parentId": 3
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+        expect(flatTreeToNestedTree(data)).toEqual(expectedData)
+    });
+
+    it('should ', () => {
+        const data = [
+            {
+                "id": 1,
+                "parentId": null,
+                "children": [
+                    {
+                        "children": [],
+                        "id": 2,
+                        "parentId": 1
+                    },
+                    {
+                        "id": 3,
+                        "parentId": 1,
+                        "children": [
+                            {
+                                "children": [],
+                                "id": 4,
+                                "parentId": 3
+                            }
+                        ]
+                    }
+                ]
+            }
+        ];
+
+        const expectedData = [
+            {
+                "id": 1,
+                "parentId": null
+            },
+            {
+                "id": 2,
+                "parentId": 1
+            },
+            {
+                "id": 3,
+                "parentId": 1
+            },
+            {
+                "id": 4,
+                "parentId": 3
+            }
+        ]
+        expect(nestedTreeToFlatTree(data)).toEqual(expectedData)
     });
 })
